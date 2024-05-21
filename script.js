f3fad05dc40571d263346455feb2656bd69402e3
Dario Matijak <darix135@gmail.com>
2024-05-21 20:12:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const boja = "";
+switch (boja) {
+  case "zuta":
+    console.log("Odabrana boja: Zuta");
+    break;
+  case "crvena":
+    console.log("Odabrana boja: Crvena");
+    break;
+  case "zelena":
+    console.log("Odabrana boja: Zelena");
+    break;
+  default:
+    console.log("neam");
+}
